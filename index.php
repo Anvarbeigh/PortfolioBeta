496d54f9e3ff09ba2f3a942b7c4169da8a7fa311
Anvarbeigh <75820113+Anvarbeigh@users.noreply.github.com>
2021-02-14 20:44:25 +0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,153 @@
+<!doctype html>
+<html lang="en-US">
+<head>
+<title>Anvarbek JU</title>
+<meta charset="utf-8">
+<meta name="description" content="Authobiography of well-known person Anvarbek Jakbaraliyev Ulug`bek o`g'li who is the studen of Westminster Academic Lyceum">
+<meta name="keywords" content="Anvar, Anvarbek, Jakbaraliyev, Anvarbek jakbaraliyev, anvarbek, anvarbeigh, a pycoder, apycoder, ajscuber, anikcubes, anik cubes">
+<meta name="author" content="Anvarbek">
+<meta name="copyright" content="Anvarbek">
+<meta name="google-site-verification" content="cO1o6sx54cvKxhbnYsABWtl4sYFj9uVKV0DxLKZkWv8"/>
+<?php include 'lib/module/sys-meta.php';?>
+<?php include 'lib/module/sys-css.php';?>
+<?php include 'lib/module/sys-js.php';?>
+<script data-cfasync="false" src='/lib/js/carousel.js?v=1'></script>
+</head>
+<body>
+<img src="/img/graphics/landing/me1.png" width="1" height="1" alt="Preload"/>
+<img src="/img/videos/1.jpg" width="1" height="1" alt="Preload"/>
+<img src="/img/videos/1b.jpg" width="1" height="1" alt="Preload"/>
+<img src="/img/videos/2.jpg" width="1" height="1" alt="Preload"/>
+<img src="/img/videos/2b.jpg" width="1" height="1" alt="Preload"/>
+<img src="/img/videos/3.jpg" width="1" height="1" alt="Preload"/>
+<img src="/img/videos/3b.jpg" width="1" height="1" alt="Preload"/>
+<?php include 'lib/module/sys-php.php';?>
+<?php include 'lib/module/ui-main-video.php'?>
+<?php
+	preloadVideo(
+	'1',
+	'8FTQgFT6Znw',
+	'1'
+	);
+	preloadVideo(
+	'2',
+	'MhLteF_xVi0',
+	'2'
+	);
+	preloadVideo(
+	'3',
+	'p_gvO2jBWwI',
+	'3'
+	);
+?>
+<div class="page-con-content landing-con-override">
+		<div class="wavebar-con-container-master">
+			<div class="wavebar-con-wrap">
+			  <div class="wavebar-svg-object"></div>
+			  <div class="wavebar-svg-object"></div>
+			</div>
+		</div>
+	<div class='landing-ico-scrolldown'>
+	</div>
+	<div class="landing-con-main">
+		<div id="object-particles">
+		</div>
+		<div class="content-con-backdrop darkmode-header">
+		</div>
+		<div class="content-con-outside">
+			<div class="content-con-inside">
+				<div class='landing-con-left content-expand '>
+					<div class="landing-con-container scale-content-txt-1 fade-in-onload">
+						<div class='landing-ico-logo'>
+						</div>
+						<div class='landing-tx1-heading pulsate'>
+							<h1>HELLO WORLD!</h1>
+						</div>
+						<div class='landing-tx2-heading'>
+							<p>
+								My name is Anvar. I am 16 years old. I am a student of Academic Lyceum under Westminster International University in Tashkent. My hobbies are programming, playing football and speedcubing.
+								I am the founder of YouTube channel called Anik Cubes.
+							</p>
+							
+						</div>
+					</div>
+				</div>
+				<div class='landing-con-right content-remove '>
+					<div class='landing-img-hero' style="background: url(/img/graphics/landing/me1.png) no-repeat center; background-size: contain;">
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	
+	
+	<div class="landing-con-contribute darkmode-feature">
+		<div class="content-con-outside">
+			<div class="content-con-inside">
+				<div class="github-con-container">
+					<div>
+						<img src="\img\graphics\landing\python.png" height="200px" align="left">
+					<div class="github-con-mantra">
+						<div class="github-wrp-mantra scale-content-txt-1 ">
+							<div class="github-tx1-mantra darkmode-txt">
+								<h2>About programming</h2>
+							</div>
+							<div class="github-tx2-mantra darkmode-txt">
+								<p>
+									 My interests to programming appeared in 2020, when I by mistake clicked an advertisement while seraching information from the NET. I clicked and saw the opportunities which gave the programming for people. So I started to learn a Python language and I am still learning. If you want to download it click the bottom below.
+								</p>
+							</div>
+							<a href="https://python.org" target="_blank">
+							<div class="github-btn-button">
+								<div class="github-tx1-button">
+									<p align="center">Download</span>
+								</div>
+							</div>
+							</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	
+	<div class="landing-con-discord darkmode-discord">
+		<div class="content-con-outside">
+			<div class="content-con-inside">
+				<div class="discord-con-container">
+					<div class="discord-con-logo">
+						<div class="discord-img-logo">
+						</div>
+					</div>
+					<div class="discord-con-mantra">
+						<div class="discord-wrp-mantra scale-content-txt-1">
+							<div class="discord-tx1-mantra darkmode-txt">
+								<h2>The Community</h2>
+							</div>
+							<div class="discord-tx2-mantra darkmode-txt">
+								<p>
+									 We use Discord to house our community. Discord allows us to keep our conversations open and accessible for newcomers seeking various information about the project, how it works and how to get the emulator up and running on your machine.
+								</p>
+							</div>
+							<a href="https://discord.me/rpcs3" target="_blank">
+							<div class="discord-btn-button">
+								<div class="discord-ico-button">
+								</div>
+								<div class="discord-tx1-button">
+									<span>Join The Discussion</span>
+								</div>
+							</div>
+							</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<svg class="discord-con-animate" preserveaspectratio="none" width="1920" height="100%" viewbox="0 0 1920 330">
+		<path class="discord-svg-animate" fill="#fff" fill-opacity="0.1" d="M140.881198,194.260295 C257.600568,129.32862 342.939626,119.84993 418.009939,203.154617 C493.080251,286.459305 545.728689,70.9046172 636.439626,63.9593047 C727.150564,57.0139922 768.99822,139.670242 858.802907,119.431961 C948.607595,99.1936797 1071.91228,-32.9977266 1243.91228,7.75227342 C1415.91228,48.5022734 1404.10369,208.584305 1508.27166,178.709305 C1612.43963,148.834305 1633.73291,79.913472 1711.63588,98.8569055 C1776.28676,114.577866 1819.96778,221.391836 1889.37253,185.808108 C2017.32661,120.206212 2004.01952,336.769569 2004.01952,336.769569 L271.635881,337 L-149.063338,337 C-149.063338,337 -245.850307,175.637635 -58.0633382,228.867188 C33.8652851,254.92501 64.1722713,236.933925 140.881198,194.260295 Z"></path>
+		</svg>
+	</div>
+	<?php include 'lib/module/ui-main-footer.php';?>
+</div>
+</body>
+</html>
